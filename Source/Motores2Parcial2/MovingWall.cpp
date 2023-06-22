245d8da176461a815a5be7cf202e26f43ae366fd
Gefeyafa <gerardo.cervantes@uniat.edu.mx>
2023-06-22 03:13:16 -0700

    Avance 2Nivel, objeto con c++, 2do recolectable

---
+++ Source/Motores2Parcial2/MovingWall.cpp
@@ -0,0 +1,50 @@
+// MovingWall.cpp
+
+#include "MovingWall.h"
+
+AMovingWall::AMovingWall()
+{
+    PrimaryActorTick.bCanEverTick = true;
+    MovementDistance = 150.0f;
+    MovementSpeed = 100.0f;
+    bMovingUp = true;
+}
+
+void AMovingWall::BeginPlay()
+{
+    Super::BeginPlay();
+
+    StartLocation = GetActorLocation();
+    TargetLocation = StartLocation + FVector(0.0f, 0.0f, MovementDistance);
+}
+
+void AMovingWall::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    FVector CurrentLocation = GetActorLocation();
+    FVector NewLocation = CurrentLocation;
+
+    float MovementDelta = MovementSpeed * DeltaTime;
+
+    if (bMovingUp)
+    {
+        NewLocation.Z += MovementDelta;
+        if (NewLocation.Z >= StartLocation.Z + MovementDistance)
+        {
+            NewLocation.Z = StartLocation.Z + MovementDistance;
+            bMovingUp = false;
+        }
+    }
+    else
+    {
+        NewLocation.Z -= MovementDelta;
+        if (NewLocation.Z <= StartLocation.Z)
+        {
+            NewLocation.Z = StartLocation.Z;
+            bMovingUp = true;
+        }
+    }
+
+    SetActorLocation(NewLocation);
+}
---
+++ Source/Motores2Parcial2/MovingWall.h
@@ -0,0 +1,31 @@
+// MovingWall.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingWall.generated.h"
+
+UCLASS()
+class MOTORES2PARCIAL2_API AMovingWall : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AMovingWall();
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void Tick(float DeltaTime) override;
+
+private:
+    UPROPERTY(EditAnywhere, Category = "Movement")
+    float MovementDistance;
+
+    UPROPERTY(EditAnywhere, Category = "Movement")
+    float MovementSpeed;
+
+    FVector StartLocation;
+    FVector TargetLocation;
+    bool bMovingUp;
+};
